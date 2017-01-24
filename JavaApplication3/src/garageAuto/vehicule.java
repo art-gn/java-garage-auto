6ce5b0d7868385c58863f66302637491d3cd0523
Arthur <arthurgandon@hotmail.fr>
2017-01-24 16:28:12 +0100

    Solution anti taquet

---
+++ JavaApplication3/src/garageAuto/vehicule.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package garageAuto;
+
+/**
+ *
+ * @author arthur
+ */
+public class vehicule {
+    int panneOuPas=0;
+    vehicule(int panneOuPas){
+        this.panneOuPas=panneOuPas;
+    }
+    vehicule(){}
+    public setVehicule(this.panneOuPas){
+        this.panneOuPas=1;
+                
+    }
+}
